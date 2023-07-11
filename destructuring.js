b0a11e2054a79839bf8631d94cea8465b3d1b960
erincunningham7 <101681089+erincunningham7@users.noreply.github.com>
2023-07-11 11:55:53 +0000

    Destruct object

--- destructuring.js
+++ destructuring.js
@@ -15,6 +15,14 @@ let [john, mary, joe] = ages;
 console.log(john, mary, joe);
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia)
 
 // Destructuring subsets
 
